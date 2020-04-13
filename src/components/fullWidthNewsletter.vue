3d768b29b76c1cb20a4e6db669fbaedf01ecfbe0
Rasul Kireev <me@rasulkireev.com>
2020-04-13 08:03:26 -0400

    Fixing Forms

--- src/components/fullWidthNewsletter.vue
+++ src/components/fullWidthNewsletter.vue
@@ -5,7 +5,16 @@
         </p>
 
         <div class="mb-2">
-          <form id="" @submit.prevent="subscribeEmail" class="flex flex-col md:flex-row" method="post" enctype="multipart/form-data" action="">
+          <form 
+          name= "add-subscriber"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field" 
+          @submit.prevent="subscribeEmail" 
+          class="flex flex-col md:flex-row" 
+          enctype="multipart/form-data" 
+          action="">
+            <input type="hidden" name="form-name" value="add-subscriber" />
             <input type="email" v-model="email" name="user_email" class="w-full p-1 mb-2 leading-tight text-gray-800 bg-gray-200 border border-gray-500 rounded appearance-none md:h-10 focus:outline-none focus:bg-white md:w-64" required="" id="id_user_email">
             <button type="submit" name="button" class="w-full text-lg font-semibold text-center text-white no-underline bg-green-500 border border-green-500 rounded cursor-pointer md:ml-2 md:h-10 sm:w-32">Subscribe</button>
           </form>
@@ -19,10 +28,12 @@ import axios from 'axios';
 const emailForm = document.getElementById("emailForm")
 
 export default {
+    name: "emailSubscriberForm",
+
     data() {
         return {
-        email: '',
-        show: false,
+            email: '',
+            show: false,
         }
     },
 
