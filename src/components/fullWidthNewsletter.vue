fc572a927b452f9c0765036f74db2806d72478e0
Rasul Kireev <me@rasulkireev.com>
2020-04-13 07:51:58 -0400

    Fixing Forms

---
+++ src/components/fullWidthNewsletter.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="emailForm" class="p-4 mb-6 border-2 border-green-300 rounded">
+        <p class="mb-2">
+          Finally, consider signing up for my personal newsletter. I will update you on the latest articles and any interesting articles and resources I've encountered.
+        </p>
+
+        <div class="mb-2">
+          <form id="" @submit.prevent="subscribeEmail" class="flex flex-col md:flex-row" method="post" enctype="multipart/form-data" action="">
+            <input type="email" v-model="email" name="user_email" class="w-full p-1 mb-2 leading-tight text-gray-800 bg-gray-200 border border-gray-500 rounded appearance-none md:h-10 focus:outline-none focus:bg-white md:w-64" required="" id="id_user_email">
+            <button type="submit" name="button" class="w-full text-lg font-semibold text-center text-white no-underline bg-green-500 border border-green-500 rounded cursor-pointer md:ml-2 md:h-10 sm:w-32">Subscribe</button>
+          </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const emailForm = document.getElementById("emailForm")
+
+export default {
+    data() {
+        return {
+        email: '',
+        show: false,
+        }
+    },
+
+    methods: {
+        subscribeEmail() {
+            axios({
+                method: 'POST',
+                url: `https://cors-anywhere.herokuapp.com/https://emailoctopus.com/api/1.5/lists/${process.env.GRIDSOME_OCTO_LIST_ID}/contacts`,
+                data: {
+                    "api_key": process.env.GRIDSOME_EMAILOCTOPUS_API,
+                    "email_address":  this.email,
+                },
+            })
+            .then(function (response) {
+            })
+            .catch(function (error) {
+            });
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
