e08d9967c06916ded0820792ae5514b71ad7b9ce
Rasul Kireev <me@rasulkireev.com>
2020-04-13 08:14:41 -0400

    Fixing Forms

--- src/components/fullWidthNewsletter.vue
+++ src/components/fullWidthNewsletter.vue
@@ -15,7 +15,7 @@
           enctype="multipart/form-data" 
           action="">
             <input type="hidden" name="form-name" value="add-subscriber" />
-            <input type="email" v-model="email" name="user_email" class="w-full p-1 mb-2 leading-tight text-gray-800 bg-gray-200 border border-gray-500 rounded appearance-none md:h-10 focus:outline-none focus:bg-white md:w-64" required="" id="id_user_email">
+            <input type="email" v-model="userEmail" name="user_email" class="w-full p-1 mb-2 leading-tight text-gray-800 bg-gray-200 border border-gray-500 rounded appearance-none md:h-10 focus:outline-none focus:bg-white md:w-64" required="" id="id_user_email">
             <button type="submit" name="button" class="w-full text-lg font-semibold text-center text-white no-underline bg-green-500 border border-green-500 rounded cursor-pointer md:ml-2 md:h-10 sm:w-32">Subscribe</button>
           </form>
         </div>
@@ -24,8 +24,6 @@
 
 <script>
 import axios from 'axios';
-
-const emailForm = document.getElementById("emailForm")
 
 export default {
     name: "emailSubscriberForm",
@@ -44,7 +42,7 @@ export default {
                 url: `https://cors-anywhere.herokuapp.com/https://emailoctopus.com/api/1.5/lists/${process.env.GRIDSOME_OCTO_LIST_ID}/contacts`,
                 data: {
                     "api_key": process.env.GRIDSOME_EMAILOCTOPUS_API,
-                    "email_address":  this.email,
+                    "email_address":  this.userEmail,
                 },
             })
             .then(function (response) {
