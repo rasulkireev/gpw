82972161e773de8ea8a871abf67598ea6c3051d7
Rasul Kireev <me@rasulkireev.com>
2020-04-13 14:50:47 -0400

    Fixing Forms

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,13 +1,13 @@
-import axios from "axios";
-
 exports.handler = async function(event) {
+    import axios from "axios";
+
     console.log(event.body)
     const email = JSON.parse(event.body).payload.userEmail
     console.log(`Recieved a submission: ${email}`)
 
     axios({
         method: 'POST',
-        url: `https://cors-anywhere.herokuapp.com/https://emailoctopus.com/api/1.5/lists/${process.env.GRIDSOME_OCTO_LIST_ID}/contacts`,
+        url: `https://emailoctopus.com/api/1.5/lists/${process.env.OCTO_LIST_ID}/contacts`,
         data: {
             "api_key": process.env.EMAILOCTOPUS_API,
             "email_address":  email,
