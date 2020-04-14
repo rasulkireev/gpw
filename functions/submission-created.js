2f35610a2a80cf71d6f89f44b619b681a26a7b52
Rasul Kireev <me@rasulkireev.com>
2020-04-14 10:34:52 -0400

    Almost there

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -5,7 +5,7 @@ exports.handler = async function(event, context) {
     const email = JSON.parse(event.body).payload.email
     console.log(`Recieved a submission: ${email}`)
 
-    await axios({
+    return await axios({
         method: 'POST',
         url: `https://emailoctopus.com/api/1.5/lists/${process.env.OCTO_LIST_ID}/contacts`,
         data: {
