b4ffd51741e17af6ee83f620f02f8ddb117b3f4c
Rasul Kireev <me@rasulkireev.com>
2020-04-13 23:10:20 -0400

    FIx Form

--- src/components/fullWidthNewsletter.vue
+++ src/components/fullWidthNewsletter.vue
@@ -14,7 +14,7 @@
           @submit.prevent="handleFormSubmit"
           class="flex flex-col md:flex-row">
             <input type="hidden" name="form-name" value="add-subscriber" />
-            <input type="email" v-model="userEmail" name="user_email" class="w-full p-1 mb-2 leading-tight text-gray-800 bg-gray-200 border border-gray-500 rounded appearance-none md:h-10 focus:outline-none focus:bg-white md:w-64" required="" id="id_user_email">
+            <input type="email" name="email" v-model="userEmail" class="w-full p-1 mb-2 leading-tight text-gray-800 bg-gray-200 border border-gray-500 rounded appearance-none md:h-10 focus:outline-none focus:bg-white md:w-64">
             <button type="submit" name="button" class="w-full text-lg font-semibold text-center text-white no-underline bg-green-500 border border-green-500 rounded cursor-pointer md:ml-2 md:h-10 sm:w-32">Subscribe</button>
           </form>
         </div>
@@ -41,12 +41,17 @@ export default {
             },
 
         handleFormSubmit(e) {
+            const axiosConfig = {
+                header: { "Content-Type": "application/x-www-form-urlencoded" }
+            };
+
             axios.post(
                 location.href, 
                 this.encode({
                     'form-name': e.target.getAttribute("name"),
                     ...this.userEmail,
-                }),    
+                }),
+                axiosConfig
             )
             .then(data => console.log(data))
             .catch(error => console.log(error))
