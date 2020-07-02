e3efeb273a5805742d6fb246df67139a437eb11a
Rasul Kireev <me@rasulkireev.com>
2020-07-02 13:15:10 -0400

    Fixed Schema on the articles page

--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -3,13 +3,15 @@
         <section>
           <div v-for="post in $page.posts.edges" :key="post.id" class="mb-1">
                 <g-link :to="post.node.path" class="flex flex-row items-center p-1 text-xl text-gray-900 border-0 rounded-lg hover:bg-gray-200" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
-                    <g-image :src="post.node.icon" class="self-start inline w-16 h-16 p-2 mr-4 align-middle" />
+                    <g-image :src="post.node.icon" class="self-start inline w-16 h-16 p-2 mr-4 align-middle" itemprop="image" />
                     <div class="">
                         <p class="text-sm md:text-lg">{{ post.node.title }}</p>
-                        <p class="m-0 text-xs text-gray-600">
-                          {{ post.node.description }}
-                          <time class="dt-published" itemprop="dateCreated" :datetime="post.node.date"></time>
-                        </p>
+                        <p class="m-0 text-xs text-gray-600" itemprop="headline">{{ post.node.description }}</p>
+                        <div id="blog-post-info" class="hidden">
+                          <time class="dt-published" itemprop="datePublished" :datetime="post.node.date">{{post.node.date}}</time>
+                          <span itemprop="author">Rasul Kireev</span>
+                          <a href="https://rasulkireev.com" itemprop="publisher">https://rasulkireev.com</a>
+                        </div> 
                     </div>
                 </g-link>
             </div>
