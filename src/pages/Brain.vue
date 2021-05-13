159cbcf272174d74ac9ad9c76466f64a95c553f6
Rasul Kireev <rasul.kireev@guycarp.com>
2021-05-13 14:31:02 -0400

    got a list of categories in brain displayed

--- src/pages/Brain.vue
+++ src/pages/Brain.vue
@@ -1,17 +1,25 @@
 <template>
-  <Layout>
-      <div class="p-2 mb-4 bg-red-400 border-l-4 border-red-800 rounded">
-        <p class="text-lg text-red-800">This page is still under construction.</p>
-      </div>
+  <Layout>  
 
-      <div v-for="note in $page.brainNotes.edges" :key="note.id" class="mb-1">
+      <ul class="flex flex-row flex-wrap">
+        <li 
+          v-for="(category, index) in categories" 
+          :Key="index"
+          class="p-2 m-1 rounded shadow"
+        >
+          <a class="">{{ category }}</a>
+        </li>
+      </ul>
+
+
+      <!-- <div v-for="note in $page.brainNotes.edges" :key="note.id" class="mb-1">
         <g-link :to="note.node.path" class="flex flex-row items-center p-1 text-xl text-gray-900 border-0 rounded-lg hover:bg-gray-200">
           <div class="">
             <p class="text-sm md:text-lg">{{ note.node.title }}</p>
             <p class="m-0 text-xs text-gray-600">{{ note.node.category }}</p>
           </div>
         </g-link>
-      </div>
+      </div> -->
   </Layout>
 </template>
 
@@ -33,6 +41,29 @@ query BrainNotes {
 
 <script>
 export default {
+  data () {
+    return {
+        selectedCategory: "",
+        categories: Array,
+    }
+  },
+
+  computed: {
+    load() {
+        const dataset = this.$page.brainNotes.edges
+
+        let allCategories = []
+        dataset.forEach(function (item) {
+            allCategories.push(item.node.category);
+        });
+
+        let uniqueCategories = [...new Set(allCategories)];
+        console.log(uniqueCategories)
+        
+        this.categories = uniqueCategories
+    },
+  },
+  
   metaInfo: {
     title: 'Brain',
     description: "This is what my brain looks like. Just kidding, that's the contents of my Joplin Notebook (a.k.a Second Brain)",
